0ef8d2ae1c07abf0a51f1a26e0b3d5ba9828aadf
Sergey Bakin <sergeyb@local.pushok.com>
2017-02-14 14:42:37 +0300

    added conection to DB

--- app.js
+++ app.js
@@ -1,61 +1,8 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var lessMiddleware = require('less-middleware');
-var  hbs = require('hbs');
-var config = require('./config');
 
-
-var app = express();
-var self = this;
-self.app = app;
-
-hbs.registerPartials(__dirname + '/views');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function (req, res, next) {
-	res.locals.tiles = config.app.tiles;
-	res.locals.mapOpts = config.app.mapOpts;
-	next();
+var main = require('modules');
+var app = main();
+app.startApp(function (err) {
+	if (err) {
+		console.log(err);
+	}
 });
-//require router
-require('./routes/index')(self);
-
-app.listen(process.env.PORT || config.app.port, function () {
-	console.log('Example app listening on port', process.env.PORT || config.app.port)
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
-
-module.exports = app;
